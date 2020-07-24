0596b6f76366011292e4a5f3f3e78e7f0c3f018f
smr89 <smir.sergio@gmail.com>
2020-07-24 16:57:00 +0200

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "nat" {
-  ami                         = "ami-0ba1c3ef9156479a2"
-  availability_zone           = "eu-west-1a"
+  ami                         = "ami-0ecfb3a630c79bc6e"
+  availability_zone           = "ca-central-1"
   instance_type               = "t2.micro"
   key_name                    = var.aws_key_name
   vpc_security_group_ids      = [aws_security_group.nat.id]
@@ -15,7 +15,7 @@ resource "aws_instance" "nat" {
 
 resource "aws_instance" "web-1" {
   ami                         = var.amis[var.aws_region]
-  availability_zone           = "eu-west-1a"
+  availability_zone           = "ca-central-1"
   instance_type               = "t2.micro"
   key_name                    = var.aws_key_name
   vpc_security_group_ids      = [aws_security_group.web.id]
@@ -30,7 +30,7 @@ resource "aws_instance" "web-1" {
 
 resource "aws_instance" "db-1" {
   ami                    = var.amis[var.aws_region]
-  availability_zone      = "eu-west-1a"
+  availability_zone      = "ca-central-1"
   instance_type          = "t2.micro"
   key_name               = var.aws_key_name
   vpc_security_group_ids = [aws_security_group.db.id]
