ed068578d0669108e243d7048002fbfa383ecb06
smr89 <smir.sergio@gmail.com>
2020-07-24 16:38:34 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -56,7 +56,7 @@ resource "aws_subnet" "eu-west-1a-public" {
   vpc_id = aws_vpc.main_vpc.id
 
   cidr_block        = var.public_subnet_cidr
-  availability_zone = "eu-west-1a"
+  availability_zone = "ca-central-1a"
 
   tags = {
     Name = "Public Subnet"
@@ -67,7 +67,7 @@ resource "aws_subnet" "eu-west-1a-private" {
   vpc_id = aws_vpc.main_vpc.id
 
   cidr_block        = var.private_subnet_cidr
-  availability_zone = "ca-central-1"
+  availability_zone = "ca-central-1a"
 
   tags = {
     Name = "Private Subnet"
